55a2a15a9095e44c99f260e42fc92cb9318dd6a6
Xinonria <znysg.outlook.com>
2019-01-26 12:09:08 +0800

    天梯2A AC :3

--- Skyup/2A.cpp
+++ Skyup/2A.cpp
@@ -5,29 +5,29 @@
 
 using namespace std;
 
-bool h[1000001],l[1000001];
+bool h[1000001], l[1000001];
 
 int main()
 {
-	int n,m,k;
-	cin>>n>>m>>k;
-	int temp_h,temp_l;
-	for(int i=1;i<=k;++i)
+	int n, m, k;
+	cin >> n >> m >> k;
+	int temp_h, temp_l;
+	for (int i = 1; i <= k; ++i)
 	{
-		scanf("%d%d",&temp_h,&temp_l);
-		h[temp_h]=true;
-		l[temp_l]=true;
+		scanf("%d%d", &temp_h, &temp_l);
+		h[temp_h] = true;
+		l[temp_l] = true;
 	}
-	int cnt_h=0,cnt_l=0;
-	for(int i=1;i<=n;++i)
+	int cnt_h = 0, cnt_l = 0;
+	for (int i = 1; i <= n; ++i)
 	{
-		if(!h[i]) ++cnt_h;
+		if (!h[i]) ++cnt_h;
 	}
-	for(int i=1;i<=m;++i)
+	for (int i = 1; i <= m; ++i)
 	{
-		if(!l[i]) ++cnt_l;
+		if (!l[i]) ++cnt_l;
 	}
-	cout<<cnt_h<<" "<<cnt_l;
+	cout << cnt_h << " " << cnt_l;
 	return 0;
 }
 
