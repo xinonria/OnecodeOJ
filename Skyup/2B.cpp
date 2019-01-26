bae7563b7ef445819410af9864508efdfc383b4c
Xinonria <znysg.outlook.com>
2019-01-26 12:09:33 +0800

    天梯2B AC :3

--- Skyup/2B.cpp
+++ Skyup/2B.cpp
@@ -14,46 +14,46 @@ int n;
 
 void i_nit()
 {
-	for(int i=1;i<=n;++i)
+	for (int i = 1; i <= n; ++i)
 	{
-		a[i].c=0;
+		a[i].c = 0;
 	}
 }
 
-bool cmp(Test x,Test y)
+bool cmp(Test x, Test y)
 {
 	return x.money > y.money;
 }
 
 void fz(string x)
 {
-	for(int i=1;i<=n;++i)
+	for (int i = 1; i <= n; ++i)
 	{
-		if(a[i].test == x) ++a[i].c;
+		if (a[i].test == x) ++a[i].c;
 	}
 }
 
 int main()
 {
-	cin>>n;
+	cin >> n;
 	i_nit();
-	for(int i=1;i<=n;++i)
+	for (int i = 1; i <= n; ++i)
 	{
-		cin>>a[i].test>>a[i].money;
+		cin >> a[i].test >> a[i].money;
 	}
-	sort(a+1,a+n+1,cmp);
-	int sum=0;
-	int test_cnt=1;
-	for(int i=1;i<=n;++i)
+	sort(a + 1, a + n + 1, cmp);
+	int sum = 0;
+	int test_cnt = 1;
+	for (int i = 1; i <= n; ++i)
 	{
-		if(a[i].money <= 0) break;
-		if(a[i].c != 3)
+		if (a[i].money <= 0) break;
+		if (a[i].c != 3)
 		{
-			sum+=a[i].money;
+			sum += a[i].money;
 			fz(a[i].test);
 		}
 	}
-	cout<<sum<<endl;
+	cout << sum << endl;
 	return 0;
 }
 
