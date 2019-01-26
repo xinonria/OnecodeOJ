5172eb8637310564db28fc5f7148a470f2bb92e5
Xinonria <znysg.outlook.com>
2019-01-26 12:18:27 +0800

    高精度加法 AC :3

--- Class/HighPrecision/add.cpp
+++ Class/HighPrecision/add.cpp
@@ -7,23 +7,23 @@ int a[100010], b[100010];
 
 void jiafa()
 {
-	int maxx = a[0] > b[0]? a[0]:b[0];
-	for(int i = 1; i <= maxx; ++i)
+	int maxx = a[0] > b[0] ? a[0] : b[0];
+	for (int i = 1; i <= maxx; ++i)
 	{
 		a[i] = a[i] + b[i];
-		if(a[i] > 9)
+		if (a[i] > 9)
 		{
 			a[i] = a[i] - 10;
-			a[i+1] = a[i+1] + 1;
+			a[i + 1] = a[i + 1] + 1;
 		}
 	}
-	if(a[maxx + 1]) a[0] = maxx + 1;
+	if (a[maxx + 1]) a[0] = maxx + 1;
 	else a[0] = maxx;
 }
 
 void print()
 {
-	for(int i = a[0]; i >= 1; --i)
+	for (int i = a[0]; i >= 1; --i)
 	{
 		cout << a[i];
 	}
@@ -36,13 +36,13 @@ int main()
 	cin >> x >> y;
 	a[0] = x.length();
 	b[0] = y.length();
-	for(int i = 1; i <= a[0]; ++i)
+	for (int i = 1; i <= a[0]; ++i)
 	{
-		a[i] = x[a[0]-i] - '0';
+		a[i] = x[a[0] - i] - '0';
 	}
-	for(int i = 1; i <= b[0]; ++i)
+	for (int i = 1; i <= b[0]; ++i)
 	{
-		b[i] = y[b[0]-i] - '0';
+		b[i] = y[b[0] - i] - '0';
 	}
 	jiafa();
 	print();
