28e663eb4e4549f2400baae1d3827482b40a9366
xinonria <znysg@outlook.com>
2019-02-22 20:18:30 +0800

    10025 WA :3 尝试缩减长度失败

--- TestLibrary/10025.cpp
+++ TestLibrary/10025.cpp
@@ -2,50 +2,29 @@
 
 using namespace std;
 
+char grades[10] = { 'E','D','C','B','A','A','A','A','A','A' };
+
 int main()
 {
+	//目前发现总分为60会出现错误
 	int a, b;
 	cin >> a >> b;
 	switch (b)
 	{
 	case 1:
-		a += 20;
+		cout << grades[(a + 20 - 51) / 10];
 		break;
 	case 2:
-		a += 10;
+		cout << grades[(a + 10 - 51) / 10];
 		break;
 	case 3:
-		a += 5;
+		cout << grades[(a + 5 - 51) / 10];
 		break;
 	case 0:
+		cout << grades[(a + 0 - 51) / 10];
 		break;
 	default:
 		break;
 	}
-	//尝试直接一句话数组解决输出,因为题目限制导致有漏洞,啊重复而枯燥的工程对人体是有害的 cout<<grades[(a-50)/10];
-	if (a <= 59)
-	{
-		cout << "E";
-		return 0;
-	}
-	if (a <= 70)
-	{
-		cout << "D";
-		return 0;
-	}
-	if (a <= 80)
-	{
-		cout << "C";
-		return 0;
-	}
-	if (a <= 90)
-	{
-		cout << "B";
-		return 0;
-	}
-	if (a >= 91)
-	{
-		cout << "A";
-		return 0;
-	}
+	return 0;
 }
